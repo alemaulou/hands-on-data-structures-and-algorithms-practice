c6b21a04d0556d41fcf419cacaa4430b624220d3
alemaulou <alemau@uw.edu>
2019-01-23 00:16:52 -0800

    Completed first String Data Structure demo

---
+++ src/Strings/StringDemo.java
@@ -0,0 +1,24 @@
+package Strings;
+/* 
+ * Using String Data Structure for Word Processing
+ * Review methods for manipulating Strings.
+ * 
+ * String class:
+ * Data type used for storing sequences of characters.
+ * char is a primitive data type stores one character.
+ * String class - part of the java.lang package.
+ */
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String str1 = "My first string";
+        String str2 = ("My Second String"); // encapsulating in set of paranthesis, not necessary.
+        String str3 = new String ("My last string"); // this is redundant
+        
+        // Concatenation
+        String str4 = str1 + "\n" + str2 + "\n" + str3;
+        
+        System.out.println(str4);
+    }
+
+}
